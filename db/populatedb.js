ec8b68a49c2c3aee1d246c14c0972b8311a615fa
sanuarKhan <sanuarkhan220@gmail.com>
2025-04-09 13:11:34 +0600

    fix: resolve merge conflicts and clean up database connection code

--- db/populatedb.js
+++ db/populatedb.js
@@ -2,22 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const {Client} = require('pg');
 
-<<<<<<< HEAD
-// const pghost = process.env.PGHOST;
-// const pguser = process.env.PGUSER;
-// const pgdatabase = process.env.PGDATABASE;
-// const pgpassword = process.env.PGPASSWORD;
-// const pgport = process.env.PGPORT;
 const { pg_connectionString } = require('../constant.js');
-
-=======
-const pghost = process.env.PGHOST;
-const pguser = process.env.PGUSER;
-const pgdatabase = process.env.PGDATABASE;
-const pgpassword = process.env.PGPASSWORD;
-const pgport = process.env.PGPORT;
->>>>>>> 86ec19a18f002ecee9d0bcd4828c911eff2a3a6e
-
 
 
 const SQL = `
@@ -35,11 +20,10 @@ const SQL = `
 async function populate() {
     console.log(`seeding...`);
     const client = new Client({
-<<<<<<< HEAD
+
         connectionString: pg_connectionString,
-=======
-        connectionString: `postgresql://${pguser}:${pgpassword}@${pghost}:${pgport}/${pgdatabase}`
->>>>>>> 86ec19a18f002ecee9d0bcd4828c911eff2a3a6e
+
+       
     });
     await client.connect();
     await client.query(SQL);
